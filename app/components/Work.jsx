ba49655f7c672610d3cde452be10877a9a7ba459
Dakshesh Sharma <daksha9300raw@gmail.com>
2025-02-01 22:13:50 +0530

    completed project

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -50,29 +50,33 @@ const Work = ({ isDarkMode }) => {
         transition={{ duration: 0.6, delay: 0.9 }}
       >
         {workData.map((project, index) => (
-          <a 
-          key={index} 
-          href={project.link} 
-          target="_blank" 
-          rel="noopener noreferrer"
-        >
-          <motion.div
-            whileHover={{ scale: 1.05 }}
-            transition={{ duration: 0.3 }}
-            className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
-            style={{ backgroundImage: `url(${project.bgImage})` }}
+          <a
+            key={index}
+            href={project.link}
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-              <div>
-                <h2 className="font-semibold">{project.title}</h2>
-                <p className="text-sm text-gray-700">{project.description}</p>
+            <motion.div
+              whileHover={{ scale: 1.05 }}
+              transition={{ duration: 0.3 }}
+              className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
+              style={{ backgroundImage: `url(${project.bgImage})` }}
+            >
+              <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+                <div>
+                  <h2 className="font-semibold">{project.title}</h2>
+                  <p className="text-sm text-gray-700">{project.description}</p>
+                </div>
+                <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+                  <Image
+                    src={assets.send_icon}
+                    alt="send Icon"
+                    className="w-5"
+                  />
+                </div>
               </div>
-              <div className="border rounded-full border-black w-9 aspect-square flex items-center justify-center shadow-[2px_2px_0_#000] group-hover:bg-lime-300 transition">
-                <Image src={assets.send_icon} alt="send Icon" className="w-5" />
-              </div>
-            </div>
-          </motion.div>
-        </a>
+            </motion.div>
+          </a>
         ))}
       </motion.div>
 
